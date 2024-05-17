680aee662c247328c7d63147d76f8d1d8aea8278
angel <almazanluis23@gmail.com>
2024-05-17 09:25:13 -0600

    ya casi termianmos

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -2,6 +2,21 @@
 /*1️⃣ EJERCICIO 01 1️⃣*/
 
 function crearUsuario() {
+
+   return Usuario;
+}
+   class Usuario {
+      constructor (usuario, nombre, email, password) {
+         this.usuario = usuario;
+         this.nombre = nombre;
+         this.email = email;
+         this.password = password;
+      }
+   
+      saludar() {
+         return `Hola, mi nombre es ${this.nombre}`;
+      }
+   
    // Crea una Clase de ES6 o una función constructora llamada "Usuario".
    // El constructor de esta clase debe tener las propiedades: "usuario", "nombre", "email" y "password".
    // El valor de cada propiedad la recibirá por parámetro.
@@ -10,16 +25,22 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
-}
+   }
+
 
 function agregarMetodoPrototype(Usuario) {
-   // Agrega un método al prototipo de "Usuario".
+   Usuario.prototype.saludar = function() {
+      return "Hello World!";
+   };// Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
 }
 
 function agregarStringInvertida() {
-   // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
+   String.prototype.reverse = function() {
+      return this.split("").reverse().join("");
+   };
+// Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
 }
--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -1,31 +1,43 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 
 function mayuscula(nombre) {
-   // Esta función recibe un nombre (string).
+    // Esta función recibe un nombre (string).
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
+   return nombre.charAt(0).toUpperCase() + nombre.slice(1);
 }
 
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
-}
+   cb();
 
+} 
+
+ 
 function operacionMatematica(num1, num2, cb) {
-   // En este ejercicio recibirás dos números y un callback.
+   
+      // En este ejercicio recibirás dos números y un callback.
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+   return cb(num1, num2);
 }
+
 
 function sumarArray(arrayOfNumbers, cb) {
    // Recibes un arreglo de números y un callback.
    // Suma todos los números del arreglo.
    // Este resultado debes pasárselo como argumento al callback recibido.
-   // [NOTA]: no debes reotrnar nada.
+   // [NOTA]: no debes retornar nada.
    // Tu código:
+   var suma = arrayOfNumbers.reduce(function(acc, curr) {
+       return acc + curr;
+   }, 0);
+   
+   cb(suma);
 }
 
 function forEach(array, cb) {
@@ -33,19 +45,34 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+   for (var i = 0; i < array.length; i++) {
+       cb(array[i]);
+   }
 }
 
 function map(array, cb) {
-   // Debes iterar sobre el arreglo, y cada elemento pasárselo como arguemento al callback.
+   // Debes iterar sobre el arreglo, y cada elemento pasárselo como argumento al callback.
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+   var newArray = [];
+   for (var i = 0; i < array.length; i++) {
+       newArray.push(cb(array[i]));
+   }
+   return newArray;
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   var filteredArray = [];
+   for (var i = 0; i < arrayOfStrings.length; i++) {
+       if (arrayOfStrings[i][0] === "a" || arrayOfStrings[i][0] === "A") {
+           filteredArray.push(arrayOfStrings[i]);
+       }
+   }
+   return filteredArray;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
